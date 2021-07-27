44a502324d73cc0cf1cd1dfc051a754c16984a4f
Eduardo <eduardoveltroni@hotmail.com>
2021-07-27 20:15:58 -0300

    Basic project

---
+++ scripts/scripts.js
@@ -0,0 +1,5 @@
+// Campos do formulário devem ser validados tanto em obrigatoriedade quanto em formato. Além disso, os campos Telefone e CPF devem possuir máscaras;
+
+// Utilizar bibliotecas vanilla para carrousel e máscara;
+
+// Utilizar o pacote NodeJS chamado json-server para mockup da API de leads.
