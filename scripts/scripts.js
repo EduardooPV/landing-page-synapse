11d1647647f6cf557c2090f3a157aeb397bb25bc
Eduardo <eduardoveltroni@hotmail.com>
2021-08-02 16:38:41 -0300

    Create modal confirm form

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,26 +4,6 @@
 
 // Utilizar o pacote NodeJS chamado json-server para mockup da API de leads.
 
-
-var imgs = document.getElementsByClassName("img")
-
-for (var i = 0; i < imgs.length ; ++i){
-    imgs[i].onclick = function() {click()};
-  }
-
-function click() {
-    var description = document.querySelector(".description")
-
-
-    if (description.style.display === "none") {
-        description.style.display = "block"
-    } else {
-        description.style.display = "none"
-    }
-
-}
-
-
 var buttonModal = document.getElementsByClassName("button__modal")
 
 // Taking all the "TENHO INTERESSE" buttons and applying the onClick Modal Open
@@ -42,4 +22,51 @@ function closeModal() {
 }
 
 
-// CAROUSEL =============
+// Mask telefone modal
+function masktel(objeto, f) {
+    setTimeout(function() {
+        var v = mtel(objeto.value);
+        if (v != objeto.value) {
+            objeto.value = v;
+        }
+    }, 1);
+}
+
+function mtel(v) {
+    var r = v.replace(/\D/g, "");
+    r = r.replace(/^0/, "");
+    if (r.length > 10) {
+        r = r.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
+    } else if (r.length > 5) {
+        r = r.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+    } else if (r.length > 2) {
+        r = r.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+    } else {
+        r = r.replace(/^(\d*)/, "($1");
+    }
+    return r;
+}
+
+
+// Checkbox important
+function confirm() {
+    closeModal()
+
+    let modal = document.querySelector('.modal__return')
+    modal.style.display = "block"
+
+    setTimeout(function() {
+        modal.style.display = "none"
+    }, 10000)
+}
+
+function openDescription() {
+
+    if (description.style.display === "none") {
+        description.style.display = "block"
+    } else {
+        description.style.display = "none"
+    }
+    
+}
+
